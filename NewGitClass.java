9f84d1c9b06ba3e03280a80e10c7ae81ffbbd49a
Anitha <anithanambi2000@gmail.com>
2019-08-14 10:52:11 +0530

    Modified NewGitClass

--- NewGitClass.java
+++ NewGitClass.java
@@ -3,9 +3,10 @@ package ExampleArray;
 public class NewGitClass {
 
 	public static void main(String[] args) {
-		int a=3,b=6;
-		System.out.println(a+b);
-System.out.println("Hai");
+		int a=5,b=6;
+		int c=a+b;
+		System.out.println(c);
+
 	}
 
 }
